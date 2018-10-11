c6c6d9c0ff9969b940b0fd259b2c58288f6d66a5
FLANNERY-LAPTOP\Flannery <flancor@swbell.net>
2018-10-11 15:16:04 -0400

    Added initial infinite runner prototype

--- Runner/Assets/Scripts/DestroyerScript.cs
+++ Runner/Assets/Scripts/DestroyerScript.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DestroyerScript : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player")
-			Debug.Break();
+			SceneManager.LoadScene(1);
 			return;
 
 		if (other.gameObject.transform.parent)
--- Runner/Assets/Scripts/HUDScript.cs
+++ Runner/Assets/Scripts/HUDScript.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class HUDScript : MonoBehaviour {
 
-	float playerScore = 0;
+	int playerScore = 0;
 
 	// Update is called once per frame
 	void Update () {
-		playerScore += Time.deltaTime;
+		//playerScore += Time.deltaTime;
 	}
 
 	public void IncreaseScore(int amount){
 		playerScore += amount;
 	}
 
+	void OnDisable(){
+		PlayerPrefs.SetInt("Score", playerScore);
+	}
+
 	void OnGUI(){
-		GUI.Label(new Rect(10, 10, 100, 30), "Score: " + (int)(playerScore * 100));
+		GUI.Label(new Rect(10, 10, 100, 30), "Score: " + playerScore);
 	}
 }

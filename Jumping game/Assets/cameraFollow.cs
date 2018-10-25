e903ae1afaa9e707ca11640e5d99c3d072a104ef
pwin17 <pwin17@earlham.edu>
2018-10-24 20:02:16 -0400

    Jumping game OCT 24

---
+++ Jumping game/Assets/cameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+	// Update is called once per frame
+	void Update () {
+		if (target.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+	}
+}

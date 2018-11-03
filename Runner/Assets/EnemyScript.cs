666e85098bf303802dade4394bd88d923e019059
FLANNERY-LAPTOP\Flannery <flancor@swbell.net>
2018-11-03 15:11:10 -0400

    Made the runner simpler and easier, began working on getting the enemies animated so they can be incorporated in

---
+++ Runner/Assets/EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	private Animator myAnimator;
+	private float movementSpeed;
+	private bool facingRight;
+	private Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+		facingRight = true;
+		myAnimator = GetComponent<Animator> ();
+		startPos = transform.Position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeDirection(){
+		facingRight = !facingRight;
+		transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
+	}
+}

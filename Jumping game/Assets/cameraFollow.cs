9c85a38792be06e181dd4a29a34ea54a7f56f912
pwin17 <pwin17@earlham.edu>
2018-11-04 22:56:35 -0500

    The Flyer update

--- Jumping game/Assets/cameraFollow.cs
+++ Jumping game/Assets/cameraFollow.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class cameraFollow : MonoBehaviour {
 
     public Transform target;
-    public Transform destroyerPos;
-    public float smoothSpeed = .3f;
 
-    private void Start()
-    {
-        Vector3 destroyerPos = new Vector3(transform.position.x - 1, transform.position.y - 1);
-    }
-
+    
     // Update is called once per frame
     void LateUpdate () {
 		if (target.position.y > transform.position.y)

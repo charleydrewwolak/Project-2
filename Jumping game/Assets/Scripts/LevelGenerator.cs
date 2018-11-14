e344b1d84f968f8fb2975046880c8bd142ae7530
pwin17 <pwin17@earlham.edu>
2018-11-14 16:01:57 -0500

    jumper 11/14/18

--- Jumping game/Assets/Scripts/LevelGenerator.cs
+++ Jumping game/Assets/Scripts/LevelGenerator.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour {
 
     public GameObject platformPrefab;
-    //public GameObject acorn;
 
     public int numberOfPlatforms;
-    //public int numberOfAcorns;
     public float levelWidth = 3f;
     public float minY = .2f;
     public float maxY = 1.5f;
@@ -16,21 +14,13 @@ public class LevelGenerator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        Vector3 spawnPosition = new Vector3();
-        Vector3 acornPosition = new Vector3();
-
+        Vector3 spawn1Position = new Vector3();
 
         for (int i = 0; i< numberOfPlatforms; i++)
         {
-            spawnPosition.y += Random.Range(minY, maxY);
-            spawnPosition.x = Random.Range(levelWidth, -levelWidth);
-            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
-            /*acornPosition.y = spawnPosition.y + 0.4f;
-            acornPosition.x = spawnPosition.x;
-            Instantiate(acorn, acornPosition, Quaternion.identity);
-            */
+            spawn1Position.y += Random.Range(minY, maxY);
+            spawn1Position.x = Random.Range(levelWidth, -levelWidth);
+            Instantiate(platformPrefab, spawn1Position, Quaternion.identity);
         }
 	}
-	
-	
 }
